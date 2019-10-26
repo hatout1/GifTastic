5216c076084a358d854f8b148ac41e05f0926368
hatout01 <hatout1@gmail.com>
2019-10-25 23:03:04 -0700

    finished ajax functions and still need to fix btn2 class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,43 @@
 $(document).ready(function () {
     let topics = ['dogs', 'cats', 'rabbit', 'goldfish']
-    let ApiKey = 'gc9EzgySz6PJZbjrG9UIESkEs843Crby';
+    const ApiKey = 'gc9EzgySz6PJZbjrG9UIESkEs843Crby';
 
 
     for (let i = 0; i < topics.length; i++) {
-        let buttons = $('<button>' + topics[i] + '</button>')
-        buttons.appendTo('.startMenu');
+        let buttons = $('<button' + ' value="' + topics[i] + '">' + topics[i] + '</button >').addClass('btn2');
+        $('.startMenu').append(buttons);
     }
+
+    // on click function for topics buttons
+
+    $('.startMenu').on('click', '.btn2', function () {
+        console.log(topics)
+
+        $.ajax({
+            method: "GET",
+            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + ApiKey + '&q=dogs&limit=10&offset=0&rating=G&lang=en',
+            dataType: 'json'
+        }).then(data => {
+            console.log(data)
+            if (btn2.value === 'dogs') {
+                $('.images').append(`<img class="gif_image" src="https://api.giphy.com/v1/gifs/search?api_key=gc9EzgySz6PJZbjrG9UIESkEs843Crby&q=dogs&limit=10&offset=0&rating=G&lang=en"/>`);
+            }
+            console.log("not not not")
+
+        })
+
+    })
 
     //  on click function
     $('.btn').on('click', function () {
         let userTopics = [];
         const userInput = $('#input').val();
-        event.preventDefault();
         userTopics.push(userInput);
+
         //  for loop to go through the array and creat a button to each string.
         for (let i = 0; i < userTopics.length; i++) {
-            let buttons2 = $('<button>' + userTopics[i] + '</button>')
-            buttons2.appendTo('.userMenu');
+            let buttons2 = $('<button>' + userTopics[i] + '</button>').addClass('btn2').attr('Value', userTopics[i])
+            buttons2.appendTo('.startMenu');
         }
 
         // function to prevent mutli entries of each string.
@@ -26,7 +46,7 @@ $(document).ready(function () {
         // ajax function
         $.ajax({
             method: "GET",
-            url: 'https://api.giphy.com/v1/gifs/search?api_key=gc9EzgySz6PJZbjrG9UIESkEs843Crby&q=animals&limit=10&offset=0&rating=G&lang=en',
+            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + ApiKey + '&q=' + userInput + '&limit=10&offset=0&rating=G&lang=en',
             dataType: 'json'
         }).then(response => {
             let result = response.data
@@ -40,6 +60,8 @@ $(document).ready(function () {
         })
 
     });
+
+    $()
 
 
 
