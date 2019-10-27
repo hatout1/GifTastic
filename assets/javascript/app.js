d3017c2d44984dd2dcc819c4f93fc59e1b494cbe
hatout01 <hatout1@gmail.com>
2019-10-26 18:34:11 -0700

    orginized my code and fixed attribut images links to buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,30 +8,12 @@ $(document).ready(function () {
         $('.startMenu').append(buttons);
     }
 
-    // on click function for topics buttons
-
-    $('.startMenu').on('click', '.btn2', function () {
-        console.log(topics)
-        console.log($(this).attr('value'));
-        $.ajax({
-            method: "GET",
-            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + ApiKey + '&q=dogs&limit=10&offset=0&rating=G&lang=en',
-            dataType: 'json'
-        }).then(data => {
-            console.log(data)
-            if ($(this).attr('value') === 'dogs') {
-                $('.images').append(`<img class="gif_image" src="https://media0.giphy.com/media/Q9GYuPJTT8RomJTRot/giphy.webp?cid=790b7611ae06ed2c07682691550bb440d59aa7572cda864b&rid=giphy.webp"/>`);
-            } else {
-                console.log("not not not")
-            }
-        })
-
-    })
-
-    //  on click function
+    //  on click function to generate buttons.
     $('.btn').on('click', function () {
         let userTopics = [];
         const userInput = $('#input').val();
+        $('#input').val('');
+
         userTopics.push(userInput);
 
         //  for loop to go through the array and creat a button to each string.
@@ -43,26 +25,30 @@ $(document).ready(function () {
         // function to prevent mutli entries of each string.
         $('#input').empty();
 
-        // ajax function
-        $.ajax({
-            method: "GET",
-            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + ApiKey + '&q=' + userInput + '&limit=10&offset=0&rating=G&lang=en',
-            dataType: 'json'
-        }).then(response => {
-            let result = response.data
-
-            for (x = 0; x < result.length; x++) {
-                let gifurl = response.data[x].images.original.url
-                $('.images').append(`<img class="gif_image" src="${gifurl}" style = 'height:300; width:300px;'/>`);
-            }
-
-
-        })
 
     });
 
-    $()
+    // on click function for topics buttons
 
+    $('.startMenu').on('click', '.btn2', function () {
+        console.log(topics)
+        console.log($(this).attr('value'));
+        $.ajax({
+            method: "GET",
+            url: 'https://api.giphy.com/v1/gifs/search?api_key=gc9EzgySz6PJZbjrG9UIESkEs843Crby&q=' + $(this).attr('value') + '&limit=10&offset=0&rating=G&lang=en',
+            dataType: 'json'
+        }).then(data => {
+            console.log(data)
+            let imgresult = data.data
+            for (x = 0; x < imgresult.length; x++) {
+                let imgurl = data.data[x].images.original.url
+                $('.images').append(`<img class="gif_image" src="${imgurl}"/>`);
+            }
+
+        })
+
+
+    })
 
 
 });
