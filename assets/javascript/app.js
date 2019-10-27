0a97fa819b9386a8e68088082debc8dc697628f5
hatout01 <hatout1@gmail.com>
2019-10-26 20:09:19 -0700

    fixed on click animate to still / still to animate function for images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let topics = ['dogs', 'cats', 'rabbit', 'goldfish']
     const ApiKey = 'gc9EzgySz6PJZbjrG9UIESkEs843Crby';
-
+    // var imgurl;
 
     for (let i = 0; i < topics.length; i++) {
         let buttons = $('<button' + ' value="' + topics[i] + '">' + topics[i] + '</button >').addClass('btn2');
@@ -24,32 +24,39 @@ $(document).ready(function () {
 
         // function to prevent mutli entries of each string.
         $('#input').empty();
-
-
     });
 
     // on click function for topics buttons
 
     $('.startMenu').on('click', '.btn2', function () {
-        console.log(topics)
-        console.log($(this).attr('value'));
+        // console.log(topics)
+        // console.log($(this).attr('value'));
+
         $.ajax({
             method: "GET",
             url: 'https://api.giphy.com/v1/gifs/search?api_key=gc9EzgySz6PJZbjrG9UIESkEs843Crby&q=' + $(this).attr('value') + '&limit=10&offset=0&rating=G&lang=en',
             dataType: 'json'
         }).then(data => {
-            console.log(data)
+            // console.log(data)
+
             let imgresult = data.data
             for (x = 0; x < imgresult.length; x++) {
                 let imgurl = data.data[x].images.original.url
-                $('.images').append(`<img class="gif_image" src="${imgurl}"/>`);
+                let imgstill = data.data[x].images["480w_still"].url
+                $('.images').append(`<img class="gif_image" id="gif_${[x]}" data-still="${imgstill}"  data-animate="${imgurl} "src="${imgurl}"/>`);
+
+                // image on-click function that switch image from still to animate
+                $(document).on('click', '#gif_' + [x], function () {
+                    if ($(this).attr('data-state') == 'still') {
+                        $(this).attr('src', $(this).attr("data-animate"));
+                        $(this).attr('data-state', 'animate');
+                    } else {
+                        $(this).attr('src', $(this).attr("data-still"));
+                        $(this).attr('data-state', 'still');
+                    }
+                })
             }
-
         })
-
-
     })
-
-
 });
 
